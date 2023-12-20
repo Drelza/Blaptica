b13b7687fd8462089cfdcad09acb26da5cecdc9e
Drelza <joe.ezzo@yahoo.com>
2023-12-20 09:57:02 -0500

    Add player death event

--- Scripts/Events.cs
+++ Scripts/Events.cs
@@ -4,4 +4,7 @@ public partial class Events : Node
 {
     public delegate void EnemyDestroyedEventHandler(Enemy destroyedEnemy);
     public static EnemyDestroyedEventHandler EnemyDestroyed;
+
+    public delegate void PlayerDestroyedEventHandler(Enemy killer);
+    public static PlayerDestroyedEventHandler PlayerDestroyed;
 }
