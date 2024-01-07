20fd2e9662e541b7da2ae20e5d99353c0e06d78d
Drelza <joe.ezzo@yahoo.com>
2024-01-07 17:49:16 -0500

    Load TestScene into main

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -3,4 +3,18 @@ using Godot;
 
 public partial class Main : Node
 {
+    [Export]
+    public PackedScene Stage;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        LoadStage(Stage.Instantiate<Stage>());
+    }
+
+    private void LoadStage(Stage stage)
+    {
+        AddChild(stage);
+    }
 }
