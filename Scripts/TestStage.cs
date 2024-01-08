c497b88d074c3dc798bb119da4f51c4fe320e337
Drelza <joe.ezzo@yahoo.com>
2024-01-08 13:28:13 -0500

    Remove unecessary bool from TestStage.cs

--- Scripts/TestStage.cs
+++ Scripts/TestStage.cs
@@ -16,24 +16,20 @@ public partial class TestStage : Stage
 	[Export]
     public double SpawnDelay = 1;
 
-	private bool enemiesCanSpawn;
 	private PackedScene[] enemies;
 
     public override void _Ready()
 	{
 		enemies = new PackedScene[] { RedEnemyScene, BlueEnemyScene };
-		enemiesCanSpawn = true;
 		CreateTween().SetLoops().TweenCallback(Callable.From(SpawnRandomEnemy)).SetDelay(SpawnDelay);
 		CreateTween().TweenCallback(Callable.From(EndStage)).SetDelay(10);
-
-		GameEvents.GameOver += OnGameOver;
 	}
 
 	private void EndStage()
 	{
 		if (GameState.IsGameOver)
 			return;
-			
+
 		GD.Print("Test Stage Complete");
 		StageCompleted?.Invoke();
 	}
@@ -46,23 +42,17 @@ public partial class TestStage : Stage
 
     private void SpawnEnemy(PackedScene enemyScene)
 	{
-		if (enemiesCanSpawn)
-		{
-			var newEnemy = enemyScene.Instantiate<BaseEnemy>();
-			float xPos = (float)GD.RandRange(0 + Padding, 540 - Padding);
-			newEnemy.Position = new Vector2(xPos, 0);
-			AddSibling(newEnemy);
-		}
-	}
+		if (GameState.IsGameOver)
+			return;
 
-	private void OnGameOver()
-	{
-		enemiesCanSpawn = false;
+		var newEnemy = enemyScene.Instantiate<BaseEnemy>();
+		float xPos = (float)GD.RandRange(0 + Padding, 540 - Padding);
+		newEnemy.Position = new Vector2(xPos, 0);
+		AddSibling(newEnemy);
 	}
 
     protected override void Dispose(bool disposing)
     {
         base.Dispose(disposing);
-		GameEvents.GameOver -= OnGameOver;
     }
 }
